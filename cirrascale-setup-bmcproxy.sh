9551c17019317feac9ef397879427c935894d6ef
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-07-12 16:10:56 -0700

    NETBOX_SITE

--- cirrascale-setup-bmcproxy.sh
+++ cirrascale-setup-bmcproxy.sh
@@ -3,13 +3,11 @@ set -euxo pipefail
 shopt -s inherit_errexit
 # https://dougrichardson.us/notes/fail-fast-bash-scripting.html
 
-site=$1
-
 # add-apt-repository ppa:ondrej/apache2 -y # iff ubuntu 20.04
 apt-get install apache2 libapache2-mod-auth-openidc -y
 a2enmod auth_openidc proxy_http ssl
 a2ensite default-ssl
-cirrascale-render-bmcproxy-conf.py $site > /etc/apache2/sites-available/cirrascale-bmcproxy.conf
+cirrascale-render-bmcproxy-conf.py > /etc/apache2/sites-available/cirrascale-bmcproxy.conf
 a2ensite cirrascale-bmcproxy
 
 install <(cat << EOF
@@ -19,7 +17,7 @@ shopt -s inherit_errexit
 # https://dougrichardson.us/notes/fail-fast-bash-scripting.html
 
 # STEP 1 render
-cirrascale-render-bmcproxy-conf.py $site > /etc/apache2/sites-available/cirrascale-bmcproxy.conf
+cirrascale-render-bmcproxy-conf.py > /etc/apache2/sites-available/cirrascale-bmcproxy.conf
 # STEP 2 reload
 systemctl reload apache2
 EOF
