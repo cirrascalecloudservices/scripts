50ddf3af744dd4bbe63ff7ff50a51f434c4b2b21
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-07-12 06:47:52 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,4 @@
 import setuptools
-setuptools.setup(name='cirrascale-scripts', scripts=['setup-certbot.sh', 'setup-ssl-proxy.sh', 'setup-ssl-sso-proxy.sh'])
+setuptools.setup(name='cirrascale-scripts', scripts=[
+    'cirrascale-secure-conf.py', 'cirrascale-bmc-secure-conf.py',
+    'setup-certbot.sh', 'setup-ssl-proxy.sh', 'setup-ssl-sso-proxy.sh'])
