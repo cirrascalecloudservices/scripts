30304600842edb2b88b8f66955da98bf8a1b084a
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-07-12 15:33:48 -0700

    cirrascale-setup-bmcproxy.sh

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
 setuptools.setup(name='cirrascale-scripts', scripts=[
-    'cirrascale-secure-conf.py', 'cirrascale-bmc-secure-conf.py',
+    'cirrascale-setup-bmcproxy.sh', 'cirrascale-render-bmcproxy-conf.py',
     'certbot-dns-lightsail-auth.py', 'certbot-dns-lightsail-clean.py',
     'setup-certbot.sh', 'setup-ssl-proxy.sh', 'setup-ssl-sso-proxy.sh'], requires=['pynetbox'])
