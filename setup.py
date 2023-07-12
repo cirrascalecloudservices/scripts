45b9e401beaf48f194a38991b0564f6e9feb3e5f
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-07-12 08:14:08 -0700

    auth/clean

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
 setuptools.setup(name='cirrascale-scripts', scripts=[
     'cirrascale-secure-conf.py', 'cirrascale-bmc-secure-conf.py',
+    'certbot-dns-lightsail-auth.py', 'certbot-dns-lightsail-clean.py',
     'setup-certbot.sh', 'setup-ssl-proxy.sh', 'setup-ssl-sso-proxy.sh'])
